0aaba529573bcfb0bac39fec5423cf384f0f3ac8
Kavinda Bandara Medagoda <108758296+Kavinda-B-Medagoda@users.noreply.github.com>
2023-09-11 18:30:04 +0530

    first page frontend all are done

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Container, Row, Button } from "reactstrap";
 import { NavLink, Link } from "react-router-dom";
 
@@ -21,8 +21,27 @@ const Nav__link = [
 ];
 
 function Header() {
+  const headerRef = useRef(null);
+
+  const stickyheaderFunc = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    stickyheaderFunc();
+    return window.removeEventListener("scroll", stickyheaderFunc);
+  });
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Container>
         <Row>
           <div className="nav__wrapper d-flex align-items-center justify-content-between">
--- tour-management/frontend/src/pages/Home.jsx
+++ tour-management/frontend/src/pages/Home.jsx
@@ -7,10 +7,14 @@ import heroImg2 from "../assets/images/hero-img02.jpg";
 import heroVdo from "../assets/images/hero-video.mp4";
 import Subtitle from "../shared/Subtitle";
 import WorldImg from "../assets/images/world.png";
+import Experience from "../assets/images/experience.png";
 
 import SearchBar from "../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
 import FeaturedTourList from "../components/Featured-tour/FeaturedTourList";
+import MasonryImagesgallery from "../components/image-gallery/MasonryImagesgallery";
+import Testimonial from "../components/Testimonial/Testimonial";
+import Newsletter from "../shared/Newsletter";
 
 function Home() {
   return (
@@ -79,6 +83,95 @@ function Home() {
             <FeaturedTourList />
           </Row>
         </Container>
+      </section>
+
+      {/* Featured section end */}
+
+      {/* experience section start */}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="experience__content">
+                <Subtitle subtitle={"Experience"} />
+
+                <h2>
+                  With our experience <br /> we will serve you{" "}
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolorem optio ea, error quia molestiae neque. Lorem, ipsum
+                  dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12K+</span>
+                  <h6>Successful Trip </h6>
+                </div>
+                <div className="counter__box">
+                  <span>2K+</span>
+                  <h6>Regular clients </h6>
+                </div>
+                <div className="counter__box">
+                  <span>15+</span>
+                  <h6>Years Experience </h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg="6">
+              <div className="experience__img">
+                <img src={Experience} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/* experience section end */}
+
+      {/* gallery section start */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <Subtitle subtitle={"Gallery"} />
+              <h2 className="gallery__title">
+                Visit our customers tour gallery
+              </h2>
+            </Col>
+            <Col lg="12">
+              <MasonryImagesgallery />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* gallery section end */}
+
+      {/* testimonials section */}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <Subtitle subtitle={"Fans Love"} />
+              <h2 className="testimonial__title">
+                What our fans saying about us
+              </h2>
+            </Col>
+            <Col lg="12">
+              <Testimonial />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* news letter section */}
+      <section>
+        <Newsletter />
       </section>
     </>
   );
